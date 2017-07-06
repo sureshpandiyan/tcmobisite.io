af6dd39fb14d5fa80d9d4ab8012d553b490043fd
sureshpandiyan <pandiyan5in@gmail.com>
2017-07-06 23:02:23 +0530

    website

---
+++ js/tc_login.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+	var login = $("#loginform");
+	var recover = $('#recoverform');
+	var speed = 400;
+
+	$('#to-recover').click(function(){
+		
+		$("#loginform").slideUp(speed);
+		$("#recoverform").fadeIn();
+	});
+	$('#to-login').click(function(){
+		
+		$("#recoverform").fadeOut();
+		$("#loginform").slideDown(speed);
+	});
+	
+    
+    if($.browser.msie == true && $.browser.version.slice(0,3) < 10) {
+        $('input[placeholder]').each(function(){ 
+       
+        var input = $(this);       
+       
+        $(input).val(input.attr('placeholder'));
+               
+        $(input).focus(function(){
+             if (input.val() == input.attr('placeholder')) {
+                 input.val('');
+             }
+        });
+       
+        $(input).blur(function(){
+            if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                input.val(input.attr('placeholder'));
+            }
+        });
+    });
+    }
+});
